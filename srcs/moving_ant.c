8b89347854866ae4b48a97ee3f7f678a540ae9e6
Thomas Cherret <tcherret@e1z4r12p3.42.us.org>
2019-03-01 15:19:22 -0800

    push

--- srcs/moving_ant.c
+++ srcs/moving_ant.c
@@ -6,10 +6,18 @@
 /*   By: tcherret <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 17:02:40 by tcherret          #+#    #+#             */
-/*   Updated: 2019/02/28 17:03:07 by tcherret         ###   ########.fr       */
+/*   Updated: 2019/03/01 15:19:12 by tcherret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem_in.h"
 
+int		moving_ant(t_farm *farm)
+{
 
+	while (graph_finished(farm) != 1)
+	{
+	
+	}
+	return (1);
+}
